2b608953c439e2d8578223d7a011a15605a6623b
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-06-24 14:23:18 -0500

    base functionality achieved.. again

--- js/index.js
+++ js/index.js
@@ -40,17 +40,11 @@ function dragPoint() {
 
 
 
-//paws off!
 
 
     let openWeatherToken = "f3846b0c841256675a19a50e484ca5ff";
-    // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
-    //api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={YOUR API KEY}
 
         $.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + newLat + "&lon=" + newLng + "&exclude=hourly&appid=f3846b0c841256675a19a50e484ca5ff&units=imperial").done(function(data) {
-        console.log(data.daily);
-        console.log(data.daily[0].temp.max);
-
 
         var weatherHTML = "";
         for(var i = 0; i <= 2; i++) {
@@ -59,10 +53,8 @@ function dragPoint() {
             weatherHTML += '<h1>' + (forecast.temp.max).toFixed(0) + '&deg;|' +
                 (forecast.temp.min).toFixed(0) + '&deg;</h1>';
             weatherHTML += '<p><strong>' + forecast.weather[0].description + ' </strong>' + '</p>';
-            // weatherHTML += '<p><strong>Humidity: </strong>' +
-            //     (data.daily.data[i].humidity * 100).toFixed(0) + "%" + '</p>';
-            // weatherHTML += '<p><strong>Dew Point: </strong>' + (data.daily.data[i].dewPoint).toFixed(0) + '&deg;</p>';
-            // weatherHTML += '<p><strong>Chance of Rain: </strong>' + (((data.daily.data[i].precipProbability) * 100).toFixed(0)) + '%</p>';
+            weatherHTML += '<p><strong>Humidity: </strong>' +(forecast.humidity).toFixed(0) + "%" + '</p>';
+            weatherHTML += '<p><strong>Dew Point: </strong>' + (forecast.dew_point).toFixed(0) + '&deg;</p>';
             weatherHTML += '</div>';
         }
 
