77158c1cad9519cba5d74920865eff254df9444e
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-06-24 14:57:53 -0500

    minimal styling

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ map.addControl(new MapboxGeocoder({
 
 var marker = new mapboxgl.Marker({
     draggable: true,
-    color: "pink"
+    color: "yellow"
 })
     .setLngLat([long, lat])
     .addTo(map);
@@ -39,9 +39,6 @@ function dragPoint() {
 
 
 
-
-
-
     let openWeatherToken = "f3846b0c841256675a19a50e484ca5ff";
 
         $.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + newLat + "&lon=" + newLng + "&exclude=hourly&appid=f3846b0c841256675a19a50e484ca5ff&units=imperial").done(function(data) {
@@ -53,6 +50,7 @@ function dragPoint() {
             weatherHTML += '<h1>' + (forecast.temp.max).toFixed(0) + '&deg;|' +
                 (forecast.temp.min).toFixed(0) + '&deg;</h1>';
             weatherHTML += '<p><strong>' + forecast.weather[0].description + ' </strong>' + '</p>';
+            weatherHTML += '<p><strong> UV Index: ' + (forecast.uvi).toFixed(0) + ' </strong>' + '</p>';
             weatherHTML += '<p><strong>Humidity: </strong>' +(forecast.humidity).toFixed(0) + "%" + '</p>';
             weatherHTML += '<p><strong>Dew Point: </strong>' + (forecast.dew_point).toFixed(0) + '&deg;</p>';
             weatherHTML += '</div>';
