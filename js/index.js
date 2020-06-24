505b60d97955bae26a0dfd1287d22f06b0f62da7
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-06-24 13:57:10 -0500

    had to switch to openweather, got some console logs working

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 "use strict";
 
+
+
 mapboxgl.accessToken = mapboxToken;
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-98.4916, 29.4252],
+    center: [-46.6, -23.68],
     zoom: 10
 });
 
-var lat = 29.424;
-var long = -98.4936;
+var lat = -46.6;
+var long = -23.68;
 
 map.addControl(new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
@@ -32,40 +34,38 @@ map.on("moveend", function() {
 
 function dragPoint() {
     var lngLat = marker.getLngLat();
-    var newLat = lngLat.lng;
-    var newLng = lngLat.lat;
+    var newLng = lngLat.lng;
+    var newLat = lngLat.lat;
 
+
+
+
+//paws off!
+
+
+    let openWeatherToken = "f3846b0c841256675a19a50e484ca5ff";
+    // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
+    //api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={YOUR API KEY}
+
+        $.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + newLat + "&lon=" + newLng + "&exclude=hourly&appid=f3846b0c841256675a19a50e484ca5ff&units=imperial").done(function(data) {
+        console.log(data.daily);
+        console.log(data.daily[0].temp.max);
+
+
+    //     var weatherHTML = "";
+    //     for(var i = 0; i <= 2; i++) {
+    //         weatherHTML += '<div class="col-3 card m-4 text-center">';
+    //         weatherHTML += '<h1>' + (data.daily.data[i].temp.max).toFixed(0) + '&deg;|' +
+    //             (data.daily.data[i].temperatureLow).toFixed(0) + '&deg;</h1>';
+    //         weatherHTML += '<p><strong>' + data.daily.data[i].summary + ' </strong>' + '</p>';
+    //         weatherHTML += '<p><strong>Humidity: </strong>' +
+    //             (data.daily.data[i].humidity * 100).toFixed(0) + "%" + '</p>';
+    //         weatherHTML += '<p><strong>Dew Point: </strong>' + (data.daily.data[i].dewPoint).toFixed(0) + '&deg;</p>';
+    //         weatherHTML += '<p><strong>Chance of Rain: </strong>' + (((data.daily.data[i].precipProbability) * 100).toFixed(0)) + '%</p>';
+    //         weatherHTML += '</div>';
+    //     }
     //
-    // function GetFormattedDate(date) {
-    //     var month = format(date .getMonth() + 1);
-    //     var day = format(date .getDate());
-    //     var year = format(date .getFullYear());
-    //     return month + "/" + day + "/" + year;
-    // }
-
-
-
-
-
-    $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/" + newLng + "," + newLat).done(function(data) {
-        console.log(data);
-
-
-        var weatherHTML = "";
-        for(var i = 0; i <= 2; i++) {
-            weatherHTML += '<div class="col-3 card m-4 text-center">';
-            // weatherHTML += '<h1>' + GetFormattedDate(data.daily.data[i].time) + '</h1>';
-            weatherHTML += '<h1>' + (data.daily.data[i].temperatureHigh).toFixed(0) + '&deg;|' +
-                (data.daily.data[i].temperatureLow).toFixed(0) + '&deg;</h1>';
-            weatherHTML += '<p><strong>' + data.daily.data[i].summary + ' </strong>' + '</p>';
-            weatherHTML += '<p><strong>Humidity: </strong>' +
-                (data.daily.data[i].humidity * 100).toFixed(0) + "%" + '</p>';
-            weatherHTML += '<p><strong>Dew Point: </strong>' + (data.daily.data[i].dewPoint).toFixed(0) + '&deg;</p>';
-            weatherHTML += '<p><strong>Chance of Rain: </strong>' + (((data.daily.data[i].precipProbability) * 100).toFixed(0)) + '%</p>';
-            weatherHTML += '</div>';
-        }
-
-        $("#insertData").html(weatherHTML);
+    //     $("#insertData").html(weatherHTML);
     });
 }
 marker.on('drag', dragPoint);
